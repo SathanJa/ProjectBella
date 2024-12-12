2718ddacd5430bca84fc6d7e9eece314b3586cb4
Sathananthan Januskar <139611014+SathanJa@users.noreply.github.com>
2024-12-12 18:27:19 +0000

    edit

--- FEO/Front.php
+++ FEO/Front.php
@@ -14,7 +14,7 @@
 
             <div class="menu">
                 <ul>
-                    <li><a href="#"><button type="button" class="btn btn-primary">HOME</button></a></li>
+                    <li><a href="Front.php"><button type="button" class="btn btn-primary">HOME</button></a></li>
                     <li><a href="About.php"><button type="button" class="btn btn-primary">ABOUT</button></a></li>
                     <li><a href="#"><button type="button" class="btn btn-primary">SERVICES</button></a></li>
                     <li><a href="#"><button type="button" class="btn btn-primary">BOOKING</button></a></li>
