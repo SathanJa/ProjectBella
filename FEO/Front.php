8e38365bf0be48b4c05f02649ba7d48d00fdd659
Sathananthan Januskar <139611014+SathanJa@users.noreply.github.com>
2025-01-17 09:27:59 +0000

    change

--- FEO/Front.php
+++ FEO/Front.php
@@ -20,7 +20,7 @@
                     <li><a href="About.php"><button type="button" class="btn btn-primary">ABOUT</button></a></li>
                     <li><a href="#"><button type="button" class="btn btn-primary">SERVICES</button></a></li>
                     <li><a href="#"><button type="button" class="btn btn-primary">BOOKING</button></a></li>
-                    <li><a href="Contact.html"><button type="button" class="btn btn-primary">CONTACT</button></a></li>
+                    <li><a href="Contact.php"><button type="button" class="btn btn-primary">CONTACT</button></a></li>
                     <!-- Logout Button -->
                     <li><a href="Login.php"><button type="button" class="btn btn-primary">LOGOUT</button></a></li>
                 </ul>
